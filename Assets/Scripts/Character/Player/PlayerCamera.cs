95bacd02a217ce2cabafff624bb9bda7e8e16211
Jack <jckzhao@yahoo.com>
2025-04-10 23:19:58 -0400

    adding player camera

--- Assets/Scripts/Character/Player/PlayerCamera.cs
+++ Assets/Scripts/Character/Player/PlayerCamera.cs
@@ -6,6 +6,12 @@ namespace TraverserProject
     {
         public static PlayerCamera Singleton;
         public Camera cameraObject;
+        PlayerManager player;
+
+        [Header("Camera Settings")]
+        private Vector3 CameraVelocity;
+        private float CameraSmoothSpeed = 1; // biggest the numer, longer it takes for camera to move to its position during movement
+
 
         private void Awake()
         {
@@ -21,6 +27,20 @@ namespace TraverserProject
         private void Start()
         {
             DontDestroyOnLoad(gameObject);
+        }
+        public void HandleAllCamereaActions()
+        {
+            if(player != null)
+            {
+
+            }
+        }
+        private void FollowPlayer()
+        {
+            Vector3 TargetCameraPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref CameraVelocity, CameraSmoothSpeed * Time.deltaTime);
+            transform.position = TargetCameraPosition;
+
+
         }
     }
 }
