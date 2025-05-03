2b14534abae4ea459ca4db481e82b41021754bf3
Jack <jckzhao@yahoo.com>
2025-05-02 23:33:53 -0400

    adding save

---
+++ Assets/Scripts/Character/Player/PlayerUI/UI_StatBar.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TravserserProject
+{
+
+    public class UI_StatBar : MonoBehaviour
+    {
+        private Slider slider;
+
+        protected virtual void Awake()
+        {
+            slider = GetComponent<Slider>();
+
+        }
+
+        public virtual void SetStat(int newValue)
+        {
+            slider.value = newValue;
+        }
+
+        public virtual void SetMaxStat(int maxValue)
+        {
+            slider.maxValue = maxValue;
+            slider.value = maxValue;
+        }
+    }
+
+}
