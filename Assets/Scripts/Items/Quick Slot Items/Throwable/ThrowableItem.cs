b37d2efab288d6ad8cd146eb094fbf2627b7abfa
Jack <jckzhao@yahoo.com>
2025-10-29 22:04:10 -0400

    adding throwables

--- Assets/Scripts/Items/Quick Slot Items/Throwable/ThrowableItem.cs
+++ Assets/Scripts/Items/Quick Slot Items/Throwable/ThrowableItem.cs
@@ -3,16 +3,37 @@ using UnityEngine;
 
 namespace TraverserProject
 {
+    [CreateAssetMenu(menuName = "Items/Consumables/Throwable")]
     public class ThrowableItem : QuickSlotItem
     {
         public override void AttemptToUseItem(PlayerManager player)
         {
-            base.AttemptToUseItem(player);
+            if (!CanIUseThisItem(player))
+                return;
+
+            if (currentItemAmount < 1)
+                return;
+
+            player.playerCombatManager.isUsingItem = true;
+
+            if (player.IsOwner)
+            {
+                player.playerAnimatorManager.PlayTargetActionAnimation(useItemAnimation, false, false, true, true, false);
+                player.playerNetworkManager.HideWeaponsServerRpc();
+            }
         }
 
         public override void SuccessfullyUseItem(PlayerManager player)
         {
             base.SuccessfullyUseItem(player);
+
+            if (player.IsOwner)
+            {
+                currentItemAmount--;
+
+                PlayerUIManager.Singleton.playerUIHudManager.SetQuickSlotItemQuickSlotIcon(player.playerInventoryManager.currentQuickSlotItem);
+
+            }
         }
 
         public override bool CanIUseThisItem(PlayerManager player)
