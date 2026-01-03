72cacaab737bb25dbe64973dbc4c73a2f502170b
Jack <jckzhao@yahoo.com>
2026-01-02 21:56:30 -0500

    added slashing vfx

--- Assets/Scripts/Items/Weapons/MeleeWeaponItem.cs
+++ Assets/Scripts/Items/Weapons/MeleeWeaponItem.cs
@@ -5,6 +5,9 @@ namespace TraverserProject
     [CreateAssetMenu(menuName = "Items/Weapons/Melee Weapon")]
     public class MeleeWeaponItem : WeaponItem
     {
+        [Header("VFX")]
+        public GameObject weaponSwingVFX;
+
         [Header("Attack Modifiers")]
         public float riposte_Attack_01_Modifier = 3.3f;
         public float backstab_Attack_01_Modifier = 3.0f;
