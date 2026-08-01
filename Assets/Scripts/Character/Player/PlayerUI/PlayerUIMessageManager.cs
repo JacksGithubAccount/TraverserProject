cf3701fd74d8f600053591dd3c915047dfeb2d23
Jack <jckzhao@yahoo.com>
2026-07-31 23:53:06 -0400

    more message system

---
+++ Assets/Scripts/Character/Player/PlayerUI/PlayerUIMessageManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TraverserProject
+{
+    public class PlayerUIMessageManager : PlayerUIMenu
+    {
+        public override void CloseMenu()
+        {
+            base.CloseMenu();
+        }
+    }
+}
